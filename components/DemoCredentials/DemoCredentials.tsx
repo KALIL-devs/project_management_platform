20eee61e986816ec1d961d76281b3f755658dbe2
KALIL-devs <kalilrahman055@gmail.com>
2026-05-03 07:37:56 +0530

    update login id

--- components/DemoCredentials/DemoCredentials.tsx
+++ components/DemoCredentials/DemoCredentials.tsx
@@ -3,7 +3,7 @@ export default function DemoCredentials() {
         <div style={{ position: "absolute", top: 0, right: 0, padding: 10, zIndex: 100 }}>
             <h2>Demo Credentials</h2>
             <p>Admin:</p>
-            <p>Email: admin@fixyads.com</p>
+            <p>Email: admin@portal.com</p>
             <p>Password: admin123</p>
             <p>Client:</p>
             <p>Email: client@portal.com</p>
